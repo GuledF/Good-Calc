c6ddebfc596ea041f29d24d46745fa1ac0f52dec
GuledF <65001710+GuledF@users.noreply.github.com>
2020-09-26 16:12:43 -0500

    Add files via upload

--- strong-c.py
+++ strong-c.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import math
+from math import *
 
 # click enter to make work
 # change ** to ^ or do other math module
@@ -10,90 +10,115 @@ root = Tk()
 root.title("Calculator!")
 root.configure(bg="dark grey")
 root.geometry("485x220")
-# root.resizable(width=False, height=False)
+root.resizable(width=False, height=False)
 
 
 def enterb(x):
     answer.config(state='normal')
     answer.delete(0, END)
 
+    screen_string = screen.get()
+    screen_string = screen_string.replace("^", "**")
+    screen_string = screen_string.replace("×", "*")
+    screen_string = screen_string.replace("x", "*")
+    screen_string = screen_string.replace("X", "*")
+    screen_string = screen_string.replace("÷", "/")
+    screen_string = screen_string.replace(",", "")
+
     try:
-        (eval(screen.get()) * 0)
+        (eval(screen_string))
 
     except Exception:
         answer.insert(0, "Enter Calculations Correctly")
         answer.config(state='disabled')
         return
 
-    answer.insert(0, round(eval(screen.get()), 4))
+    answer.insert(0, round(eval(screen_string), 4))
+
+    answer.config(state='disabled')
+
+
+def B_Input():
+    answer.config(state='normal')
+    answer.delete(0, END)
+
+    screen_string = screen.get()
+    screen_string2 = screen_string
+    screen_string = screen_string.replace("^", "**")
+    screen_string = screen_string.replace("×", "*")
+    screen_string = screen_string.replace("x", "*")
+    screen_string = screen_string.replace("X", "*")
+    screen_string = screen_string.replace("÷", "/")
+    screen_string = screen_string.replace(",", "")
+
+    try:
+        (eval(screen_string))
+
+    except Exception:
+        answer.insert(0, "Enter Calculations Correctly")
+        answer.config(state='disabled')
+        return
+
+    answer.insert(0, round(eval(screen_string), 4))
     root.clipboard_clear()
-    root.clipboard_append(eval(screen.get()))
+    root.clipboard_append(screen_string2)
     root.update()
 
     answer.config(state='disabled')
 
 
-def equals1():
+def B_Answer():
     answer.config(state='normal')
     answer.delete(0, END)
 
-    # screen.get().replace("^", "**")
+    screen_string = screen.get()
+    screen_string = screen_string.replace("^", "**")
+    screen_string = screen_string.replace("×", "*")
+    screen_string = screen_string.replace("x", "*")
+    screen_string = screen_string.replace("X", "*")
+    screen_string = screen_string.replace("÷", "/")
+    screen_string = screen_string.replace(",", "")
 
     try:
-        (eval(screen.get()) * 0)
+        (eval(screen_string))
 
     except Exception:
         answer.insert(0, "Enter Calculations Correctly")
         answer.config(state='disabled')
         return
 
-    answer.insert(0, round(eval(screen.get()), 4))
+    answer.insert(0, round(eval(screen_string), 4))
     root.clipboard_clear()
-    root.clipboard_append(screen.get())
+    root.clipboard_append(eval(screen_string))
     root.update()
 
     answer.config(state='disabled')
 
 
-def equals2():
+def B_Both():
     answer.config(state='normal')
     answer.delete(0, END)
 
-    # screen.get().replace("^", "**")
+    screen_string = screen.get()
+    screen_string2 = screen_string
+    screen_string = screen_string.replace("^", "**")
+    screen_string = screen_string.replace("×", "*")
+    screen_string = screen_string.replace("x", "*")
+    screen_string = screen_string.replace("X", "*")
+    screen_string = screen_string.replace("÷", "/")
+    screen_string = screen_string.replace(",", "")
 
     try:
-        (eval(screen.get()) * 0)
+        (eval(screen_string))
 
     except Exception:
         answer.insert(0, "Enter Calculations Correctly")
         answer.config(state='disabled')
         return
 
-    answer.insert(0, round(eval(screen.get()), 4))
+    answer.insert(0, round(eval(screen_string), 4))
     root.clipboard_clear()
-    root.clipboard_append(eval(screen.get()))
-    root.update()
-
-    answer.config(state='disabled')
-
-
-def equals3():
-    answer.config(state='normal')
-    answer.delete(0, END)
-
-    # screen.get().replace("^", "**")
-
-    try:
-        (eval(screen.get()) * 0)
-
-    except Exception:
-        answer.insert(0, "Enter Calculations Correctly")
-        answer.config(state='disabled')
-        return
-
-    answer.insert(0, round(eval(screen.get()), 4))
-    root.clipboard_clear()
-    root.clipboard_append(screen.get() + " = " + str(round(eval(screen.get()), 4)))
+    root.clipboard_append(screen_string2 + " = " + str(round(eval(screen_string), 4)))
     root.update()
 
     answer.config(state='disabled')
@@ -101,11 +126,11 @@ def equals3():
 
 screen = Entry(root, width=63, borderwidth=10, state='normal', font="times 10")
 answer = Entry(root, width=63, borderwidth=10, state='disabled', font="times 10")
-b1 = Button(root, text="Copy:Input", padx=13, pady=0, bg="grey", font="times 15", command=equals1)
-b2 = Button(root, text="Copy:Answer", padx=10, pady=0, bg="grey", font="times 15", command=equals2)
-b3 = Button(root, text="Copy:Both", padx=12, pady=0, bg="grey", font="times 15", command=equals3)
+b1 = Button(root, text="Copy:Input", padx=13, pady=0, bg="grey", font="times 15", command=B_Input)
+b2 = Button(root, text="Copy:Answer", padx=10, pady=0, bg="grey", font="times 15", command=B_Answer)
+b3 = Button(root, text="Copy:Both", padx=12, pady=0, bg="grey", font="times 15", command=B_Both)
 text1 = Label(root, text="Guleds Great Calculator!", padx=101, pady=10, bg="black", fg="yellow", font="times 20 underline")
-textexp = Label(root, text='Clicking any button or the "Enter" key will generate an answer, use ** for exponent', padx=16, pady=10, bg="black", fg="yellow", font="times 10")
+textexp = Label(root, text='Clicking any button or the "Enter" key will generate an answer', padx=72, pady=10, bg="black", fg="yellow", font="times 10")
 text2 = Label(root, text="Input:", padx=22, pady=10, bg="black", fg="yellow", font="times 10")
 text3 = Label(root, text="Answer:", padx=15, pady=10, bg="black", fg="yellow", font="times 10")
 text4 = Label(root, text="Buttons:", padx=14, pady=10, bg="black", fg="yellow", font="times 10")
